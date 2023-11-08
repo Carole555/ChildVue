1d55830ce82bf4661c4c04a344a8ede935e10d7d
Your Name <you@example.com>
2023-11-08 21:35:14 +0800

    商城部分全部完成

--- src/components/views/List.vue
+++ src/components/views/List.vue
@@ -11,7 +11,14 @@ import Commodities from './Commodities.vue'
 
 export default defineComponent({
   name: 'AppCommodity',
-  components: {Commodities}
+  components: {Commodities},
+  mounted () {
+    const childId = this.$route.query.childId
+    const hasChild = this.$route.query.hasChild
+    console.log('Child ID:', childId)
+    console.log('haschild111', hasChild)
+    // this.$router.push({name: 'commodities', query: {hasChild}})
+  }
 })
 </script>
 <style lang="less" scoped>
